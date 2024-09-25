43b4bc67c3ba8b6f843445ee7d243f4869d178de
Mohammad Khawar <mzkhawar775@gmail.com>
2024-09-25 08:23:08 -0500

    fixing 500 error with profile/password update

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -1,17 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+
+from user_profile.models import CustomUser
 
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)  # email field added to the form
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = ["username", "email", "password1", "password2"]
 
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ["username", "email"]
